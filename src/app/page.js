8966ca42a8779c39fa79d7502debb835d668114f
Ryan Sanjaya <ryansanjaya290799@gmail.com>
2024-02-03 22:30:44 +0700

    finish creating login form backbone

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,27 @@
+'use client'
+import { useState } from "react";
+
 const LoginPage = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
   return (
     <main className="flex h-screen flex-col items-center justify-center">
-
-      <div className="w-1/4 h-2/4 flex flex-col items-center bg-slate-200/20 border-2 border-gray-100 rounded-md">
+      <form onSubmit={(e)=>{e.preventDefault();}} className="w-1/4 h-2/4 flex flex-col items-center bg-slate-200/20 border-2 border-gray-100 rounded-md">
         <div className="text-3xl pt-7 pb-7 w-full text-center">
           Login
         </div>
 
         <div className="">
           Username
-          <input type="text"/>
+          <input type="text" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
         </div>
         <div className="">
           Password
+          <input type="text" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
         </div>
-
-      </div>
+        <button type="submit">Submit</button>
+      </form>
  
     </main>
   );
