f88dcf5648eeaa0ff06e7c55eba6e159683ec068
Ryan Sanjaya <ryansanjaya290799@gmail.com>
2024-04-07 23:45:45 +0700

    add redirect functionality, add styling to navigation bar

--- src/app/component/NavigationBar/index.js
+++ src/app/component/NavigationBar/index.js
@@ -1,11 +1,15 @@
-import react from "react";
 import style from './navigation.module.css';
 
 const NavigationBar = () => {
     return (
-        <nav className={style.container}>
-            <a className={style.menu}>Home</a>
-            <a className={style.menu}>Manage</a>
+        <nav id={style.navigationMenu}>
+            <div className="w-full h-1/4  border-b-[3px] text-[theme(colors.light)] flex flex-column justify-center items-center">
+                Logo
+            </div>
+            <div id={style.menuContainer}>
+                <a className={style.menu} href="/homepage">Home</a>
+                <a className={style.menu} href="/manage">Manage</a>
+            </div>
         </nav>
     );
 }
