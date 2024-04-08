0776d5b277318e59bb1b4f90917a2ce586bc741e
Ryan Sanjaya <ryansanjaya290799@gmail.com>
2024-04-08 14:18:01 +0700

    navigation menu complete

--- src/app/component/NavigationBar/index.js
+++ src/app/component/NavigationBar/index.js
@@ -3,9 +3,11 @@ import style from './navigation.module.css';
 const NavigationBar = () => {
     return (
         <nav id={style.navigationMenu}>
-            <div className="w-full h-1/4  border-b-[3px] text-[theme(colors.light)] flex flex-column justify-center items-center">
-                Logo
+            <div className="mt-[10px]"id={style.line}></div>
+            <div className="w-full h-1/4 text-[theme(colors.light)] flex flex-column justify-center items-center">
+                &Logo&
             </div>
+            <div id={style.line}></div>
             <div id={style.menuContainer}>
                 <a className={style.menu} href="/homepage">Home</a>
                 <a className={style.menu} href="/manage">Manage</a>
